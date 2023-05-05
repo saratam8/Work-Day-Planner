0f933612a1e3ae8ce8914bb7f43a2e8981269cfa
unknown <stam296@gmail.com>
2023-05-04 22:26:25 -0500

    Updated README and added screenshot of deployed application. Added comments to script.js and added a display of current time to top of webpage

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
 # Work-Day-Planner
 
+## Description
+A planner application for the work day where the user can save tasks to each hour of the work day. The present hour will be red, past hours are grey and future hours are green in color. Once the save button is clicked on, the tasks will be saved and persists on the page.
+
+Lines 1-63 and 121-130 of index.html were provided by the Northwestern Coding Bootcamp. 
+
+style.css was provided by the Northwestern Coding Bootcamp.
+
+script.js was written by Sara Tam.
+
+## Technology Used
+* Visual Studio Code
+* jQuery
+* Day.JS
+* BootStrap
+* Google Fonts API
+
+## Overview of Site
+![Screenshot of the deployed Word Day Planner application](./assets/planner%20screenshot.png)
+
+## Deployed Application
+https://saratam8.github.io/Work-Day-Planner/
+
+## Summary
+* Utilized Day.JS to obtain current day, date and time. 
+* Utilized BootStrap to create structure of html
+
+
 ## Resources
 
 https://api.jquery.com/category/selectors/attribute-selectors/
@@ -11,3 +38,6 @@ http://api.jquery.com/removeclass/
 http://api.jquery.com/on/
 
 https://api.jquery.com/category/traversing/tree-traversal/
+
+## Author Links
+https://github.com/saratam8
--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
+// Using day.js to get current day, date, and time
 var today = dayjs();
-$('#currentDay').text(today.format('dddd, MMMM D'));
+$('#currentDay').text(today.format('dddd, MMMM D h:m A'));
 
 var hour = today.format('H');
 console.log(hour);
 
+// Array of hours on planner
 var plannerHours = [9, 10, 11, 12, 1, 2, 3, 4, 5, 6];
+// 24 hour format of the planner hours
 var compareHours = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
 for (var i = 0; i <plannerHours.length; i++){
+  // If the current hour is on the planner, change class to present
   if(compareHours[i] == hour){
     var prev = i - 1;
     $(`div[id='hour-${plannerHours[i]}']`).removeClass("future").addClass("present");
+    // The following two for loops will set the timeblocks before current hour to past and timeblocks after current hour to future
     for(var j = 0; j < i; j++){
       $(`div[id='hour-${plannerHours[j]}']`).removeClass("present").addClass("past");
       $(`div[id='hour-${plannerHours[j]}']`).removeClass("future").addClass("past");
@@ -24,35 +25,40 @@ for (var i = 0; i <plannerHours.length; i++){
       $(`div[id='hour-${plannerHours[k]}']`).removeClass("past").addClass("future");
     }
   }
+  // This else if statement checks if the current hour is before work hours. If it is, then all the timeblocks are set to future
   else if (0 <= hour && hour < 9){
     for(var j = 0; j < plannerHours.length; j++){
       $(`div[id='hour-${plannerHours[j]}']`).removeClass("present").addClass("future");
       $(`div[id='hour-${plannerHours[j]}']`).removeClass("past").addClass("future");
     }
   }
+  // This else if statement checks if the current hour is after work hours. If it is, then all the timeblocks are set to past
   else if (18 < hour && hour < 24){
     for(var j = 0; j < plannerHours.length; j++){
       $(`div[id='hour-${plannerHours[j]}']`).removeClass("present").addClass("past");
       $(`div[id='hour-${plannerHours[j]}']`).removeClass("future").addClass("past");
     }
   }
+  // If text is present in the timeblock, it will be assigned to localstorage with the key in format of the timeblock id. 
   var key = "hour-" + plannerHours[i];
-  console.log("key is " + key);
   var task = localStorage.getItem(key);
-  console.log(task);
   if(task != null){
     $(`div[id='hour-${plannerHours[i]}']`).children('textarea').text(task);
   }
 }
 
+// Event listener for the save button of each timeblock
 $(".saveBtn").on("click", function (event) {
   event.preventDefault();
   // this references the object listening for the event. So in this case, this refers to the save button
-  console.log($(this).parent().attr('id'));
-  console.log($(this).siblings('textarea').val());
+  // console.log($(this).parent().attr('id'));
+  // console.log($(this).siblings('textarea').val());
 
+  // This variable will contain the id of the timeblock containing the save button that is clicked on
   var taskKey = $(this).parent().attr('id');
+  // This variable will contain the text that the user input into the timeblock
   var taskContent = $(this).siblings('textarea').val();
 
+  // Setting each timeblock's text to their corresponding key named after the timeblock's id
   localStorage.setItem(taskKey, taskContent);
 });
